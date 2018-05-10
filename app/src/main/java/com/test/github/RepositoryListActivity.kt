9eb66d98f893e3066074625bd8a7ee1cf9709cfe
kyu <justloveher79@mgail.com>
2018-05-10 17:33:57 +0900

    git init first commit

---
+++ app/src/main/java/com/test/github/RepositoryListActivity.kt
@@ -0,0 +1,12 @@
+package com.test.github
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RepositoryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_list)
+    }
+}
