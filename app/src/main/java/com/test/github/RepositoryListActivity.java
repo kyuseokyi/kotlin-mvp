a83b5761a08f15324b508eb8e53fd7b55a46d4be
kyu <justloveher79@mgail.com>
2018-05-20 22:39:28 +0900

    add normal code

--- app/src/main/java/com/test/github/RepositoryListActivity.java
+++ app/src/main/java/com/test/github/RepositoryListActivity.java
@@ -1,13 +1,69 @@
 package com.test.github;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 public class RepositoryListActivity extends AppCompatActivity {
+
+    private Spinner languageSpinner;
+    private ProgressBar progressBar;
+    private CoordinatorLayout coordinatorLayout;
+
+    private RepositoryAdapter repositoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository_list);
+
+        setupView();
+    }
+
+    private void setupView() {
+        // 툴바 설정
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Recycler View
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_repos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //repositoryAdapter = new RepositoryAdapter((Context) this, (RepositoryAdapter.OnRepositoryItemClickListener) this);
+        //recyclerView.setAdapter(repositoryAdapter);
+
+        // ProgressBar
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // SnackBar 표시에 이용한다
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+
+        // Spinner
+        languageSpinner = (Spinner) findViewById(R.id.language_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        adapter.addAll("java", "objective-c", "swift", "groovy", "python", "ruby", "c");
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 선택시 뿐만 아니라 처음에도 호출된다
+                String language = (String) languageSpinner.getItemAtPosition(position);
+                //loadRepositories(language);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
     }
 }
