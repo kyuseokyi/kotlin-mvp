4c11d3848eb20d92a0398eafac43831e5875f03b
kyu <justloveher79@mgail.com>
2018-05-30 18:14:12 +0900

    error fixed

--- app/src/main/java/com/test/github/NewGitHubReposApplication.java
+++ app/src/main/java/com/test/github/NewGitHubReposApplication.java
@@ -8,7 +8,6 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 /**
  * Created by justluvher on 2018. 5. 11..
  */
